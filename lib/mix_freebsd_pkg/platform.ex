7a4bbd99e0d99c510830076d46633f83e593fa77
Harshad Sharma <harshad@sharma.io>
2024-03-31 19:53:42 +0530

    Add specs.

--- lib/mix_freebsd_pkg/platform.ex
+++ lib/mix_freebsd_pkg/platform.ex
@@ -2,15 +2,18 @@ defmodule MixFreebsdPkg.Platform do
   @moduledoc """
   Documentation for `MixFreebsdPkg.Platform`.
   """
+  @spec freebsd?() :: boolean()
   def freebsd? do
     System.find_executable("freebsd-version") != nil
   end
 
+  @spec freebsd_version() :: binary()
   def freebsd_version do
     {output, _} = System.cmd("freebsd-version", [])
     output |> String.trim()
   end
 
+  @spec arch() :: binary()
   def arch do
     {output, _} = System.cmd("uname", ["-m"])
     output |> String.trim()
