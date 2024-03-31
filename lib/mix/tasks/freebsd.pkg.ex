2cc44e74db7427cfffb4e2bbc5aa8d305147f21d
Harshad Sharma <harshad@sharma.io>
2024-03-31 23:46:25 +0530

    Use alias to clean up code.

--- lib/mix/tasks/freebsd.pkg.ex
+++ lib/mix/tasks/freebsd.pkg.ex
@@ -1,17 +1,20 @@
 defmodule Mix.Tasks.Freebsd.Pkg do
   use Mix.Task
+  alias MixFreebsdPkg.Config
+  alias MixFreebsdPkg.Platform
+  alias MixFreebsdPkg.Templates
 
   @impl Mix.Task
   @spec run(OptionParser.argv()) :: :ok
   def run(argv) do
-    if MixFreebsdPkg.Platform.freebsd?() do
-      config = MixFreebsdPkg.Config.config(argv)
+    if Platform.freebsd?() do
+      config = Config.config(argv)
       # IO.inspect(config)
 
-      service_rc = MixFreebsdPkg.Templates.Service.render(config)
+      service_rc = Templates.Service.render(config)
       IO.puts("\nService rc file: \n\n#{service_rc}\n")
 
-      conf_files = MixFreebsdPkg.Templates.ConfFile.render(config)
+      conf_files = Templates.ConfFile.render(config)
       IO.inspect(conf_files)
     else
       IO.puts("Please run this task on FreeBSD.")
