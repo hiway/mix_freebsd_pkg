594e91633a0b4638c13ed0864c0d8c53d6f3937d
Harshad Sharma <harshad@sharma.io>
2024-04-15 17:17:53 +0530

    Add docs and examples.

--- lib/mix_freebsd_pkg/platform.ex
+++ lib/mix_freebsd_pkg/platform.ex
@@ -1,18 +1,43 @@
 defmodule MixFreebsdPkg.Platform do
   @moduledoc """
-  Documentation for `MixFreebsdPkg.Platform`.
+
+  """
+
+  @doc """
+  Returns true if the current platform is FreeBSD.
+
+  ## Examples
+
+      iex> MixFreebsdPkg.Platform.freebsd?()
+      true
   """
   @spec freebsd?() :: boolean()
   def freebsd? do
     System.find_executable("freebsd-version") != nil
   end
 
+  @doc """
+  Returns the FreeBSD version.
+
+  ## Examples
+
+      iex> MixFreebsdPkg.Platform.freebsd_version()
+      "14.0-RELEASE-p6
+  """
   @spec freebsd_version() :: binary()
   def freebsd_version do
     {output, _} = System.cmd("freebsd-version", [])
     output |> String.trim()
   end
 
+  @doc """
+  Returns machine architecture.
+
+  ## Examples
+
+      iex> MixFreebsdPkg.Platform.arch()
+      "amd64"
+  """
   @spec arch() :: binary()
   def arch do
     {output, _} = System.cmd("uname", ["-m"])
