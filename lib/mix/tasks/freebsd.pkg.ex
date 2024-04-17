e008cc473ff9a9b704999d1cbbd890a607213a06
Harshad Sharma <harshad@sharma.io>
2024-04-17 18:11:50 +0530

    Use templates

--- lib/mix/tasks/freebsd.pkg.ex
+++ lib/mix/tasks/freebsd.pkg.ex
@@ -4,18 +4,15 @@ defmodule Mix.Tasks.Freebsd.Pkg do
   """
   use Mix.Task
   alias MixFreebsdPkg.Config
-  alias MixFreebsdPkg.Pkg
+  # alias MixFreebsdPkg.Pkg
   alias MixFreebsdPkg.Platform
-  alias MixFreebsdPkg.Templates
+  alias MixFreebsdPkg.Template
 
   def run(_) do
     if Platform.freebsd?() do
       config = Config.config()
-      templates = Templates.render(config)
-      pkg_name = Pkg.create(config, templates)
-      IO.inspect(config, limit: :infinity)
-      IO.inspect(templates, limit: :infinity)
-      Mix.shell().info("Package created: #{pkg_name}")
+      templates = Template.render(config)
+      IO.inspect(templates)
     else
       Mix.shell().error("This task is only available on FreeBSD.")
     end
